1fa63797b50929a1a59410f2e9f6a59079d3bab0
PBaloyi449 <pbaloyi449@gmail.com>
2024-04-20 11:49:37 +0200

    Database connected

--- src/app/add-job/page.tsx
+++ src/app/add-job/page.tsx
@@ -14,16 +14,24 @@ async function addJob(formData: FormData){
     const description = formData.get("description")?.toString();
     const responsibilities = formData.get("responsibilities")?.toString();
     const requirements = formData.get("requirements")?.toString();
-    const link = formData.get("apply")?.toString();
+    const link = formData.get("link")?.toString();
     const closingDate = formData.get("closingDate")?.toString();
-   
+    
+    console.log("job: " +job )
+    console.log("company: " +company)
+    console.log(`location: ${location}`)
+    console.log(`description: ${description}`)
+    console.log(`responsibilities: ${responsibilities}`)
+    console.log(`requirements: ${requirements}`)
+    console.log(`link: ${link}`)
+    console.log(`closingDate: ${closingDate}`)
 
-    if (!job || !company || !location || !description|| !responsibilities || !requirements || !closingDate || !link ){
+    if (!job || !company || !location || !closingDate|| !description || !responsibilities || !requirements || !link ){
         throw  Error("Missing required fields");
     }
 
     await prisma.entry_level_Jobs.create({
-        data: {company,description,location,requirements,responsibilities,closingDate,job,link}
+        data: {company, description, location,requirements,responsibilities,closingDate,job,link}
     })
 
     redirect("/");
@@ -40,22 +48,46 @@ export default function addJobPage(){
                 placeholder="Job Title"
                 className="input-bordered input mb-3 w-full">
                 </input>
-                <textarea 
-                required
-                name="description"
-                placeholder="Description"
-                className="textarea-bordered textarea mb-3 w-full"
-                />
                 <input 
                 required
-                name="date posted"
-                placeholder="Date Posted"
+                name="company"
+                placeholder="Company"
                 className="input-bordered input mb-3 w-full">
                 </input>
                 <input 
                 required
                 name="location"
                 placeholder="Location"
+                className="input-bordered input mb-3 w-full">
+                </input>
+                <input 
+                required
+                name="closingDate"
+                placeholder="Closing Date"
+                className="input-bordered input mb-3 w-full">
+                </input>
+                <textarea 
+                required
+                name="description"
+                placeholder="Description"
+                className="textarea-bordered textarea mb-3 w-full"
+                />
+                <textarea 
+                required
+                name="responsibilities"
+                placeholder="Responsibilities"
+                className="textarea-bordered textarea mb-3 w-full"
+                />
+                <textarea 
+                required
+                name="requirements"
+                placeholder="Requirements"
+                className="textarea-bordered textarea mb-3 w-full"
+                />
+                <input 
+                required
+                name="link"
+                placeholder="How to apply"
                 className="input-bordered input mb-3 w-full">
                 </input>
                 <button className="btn btn-primary btn-block" type="submit">Add Job</button>
