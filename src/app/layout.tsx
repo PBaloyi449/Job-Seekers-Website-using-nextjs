dd605ccd064ea10688742c0e8506daaae2d1c8f6
PBaloyi449 <pbaloyi449@gmail.com>
2024-04-30 21:17:53 +0200

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
        <Navbar/>
         <main className="p-4 max-w-7xl m-auto min-w-[300px]">
         {children}
+        <Analytics />
         </main>
         <Footer />
         </body>
