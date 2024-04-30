b26c3fb35b6419510a149dd0eb4ba5905b25175f
PBaloyi449 <pbaloyi449@gmail.com>
2024-04-30 15:42:26 +0200

    exporting at the bottom

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -18,7 +18,7 @@ const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST, authOptions }
 
 
 
