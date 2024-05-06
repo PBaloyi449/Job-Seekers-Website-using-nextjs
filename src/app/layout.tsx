00143a5a998ac408cd073d3bfca9cf3c417a0592
PBaloyi449 <pbaloyi449@gmail.com>
2024-05-06 13:38:17 +0200

    fixed some minor things

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta name="google-adsense-account" content="ca-pub-5428303296729283"/>
-      {process.env.NODE_ENV === "production" && (
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5428303296729283"
      crossOrigin="anonymous"></script>
-    )}
+      <meta name="google-adsense-account" content="ca-pub-5428303296729283"/>
       </head>
       <body className={inter.className}>
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5428303296729283"
