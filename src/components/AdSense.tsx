518d949be27582cc9c9a2265f6581cb8559b7359
PBaloyi449 <pbaloyi449@gmail.com>
2024-05-20 09:24:38 +0200

    fixing the script tag

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -7,12 +7,11 @@ type AdsenseTypes = {
 
 const AdSense = ({ pId }: AdsenseTypes) => {
   return (
-    <Script
-        async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
-        crossOrigin='anonymous'
-        strategy='afterInteractive'
-    />
+    <script 
+    async 
+    src={"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}"}
+     crossOrigin="anonymous">
+     </script>
   )
 }
 
