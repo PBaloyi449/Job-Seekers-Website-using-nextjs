8a83ec0e92c0018353f9db0234096a0c79c93392
PBaloyi449 <pbaloyi449@gmail.com>
2024-05-21 13:24:45 +0200

    Fixing auto ads

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -7,11 +7,12 @@ type AdsenseTypes = {
 
 const AdSense = ({ pId }: AdsenseTypes) => {
   return (
-    <script 
-    async 
-    src={"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}"}
-     crossOrigin="anonymous">
-     </script>
+    <Script
+    async
+    src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+    crossOrigin='anonymous'
+    strategy='afterInteractive'
+/>
   )
 }
 
