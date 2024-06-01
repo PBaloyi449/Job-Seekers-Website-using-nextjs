7bca75a8c13a1aecf3f74fc19768d4b8b9ec4cf8
PBaloyi449 <pbaloyi449@gmail.com>
2024-06-01 14:02:39 +0200

    Fixing the sentences

--- src/app/jobs/[id]/page.tsx
+++ src/app/jobs/[id]/page.tsx
@@ -1,39 +1,44 @@
-import prisma from "@/lib/prisma"
-import { notFound } from "next/navigation"
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
 import Link from "next/link";
 import type { Metadata } from "next";
-import { cache} from "react";
+import { cache } from "react";
 
-interface JobPageProps{
+interface JobPageProps {
     params: {
         id: string
     }
 }
 
 const getJob = cache(async (id: string) => {
-    const job = await prisma.entry_level_Jobs.findUnique({where:{id}})
-    if(!job) notFound();
+    const job = await prisma.entry_level_Jobs.findUnique({ where: { id } });
+    if (!job) notFound();
     return job;
-})
+});
 
 export async function generateMetadata(
-    {params: {id}} : JobPageProps
+    { params: { id } }: JobPageProps
 ): Promise<Metadata> {
-    const job = await getJob(id); 
+    const job = await getJob(id);
 
     return {
         title: job.job + " - Employment Echo",
-        description: job.description, 
-    }
+        description: job.description,
+    };
 }
 
-export default async function JobPage(
-    {params: {id}} : JobPageProps
-){
-    const job = await getJob(id);
-    
+const renderSentences = (text: string) => {
+    return text.split('. ').map((sentence, index) => (
+        <p key={index}>{sentence.trim()}.</p>
+    ));
+};
 
-    return(
+export default async function JobPage(
+    { params: { id } }: JobPageProps
+) {
+    const job = await getJob(id);
+
+    return (
         <div>
             <div>
                 <h1 className="text-4xl font-bold">{job.job}</h1>
@@ -43,15 +48,15 @@ export default async function JobPage(
             </div>
             <div>
                 <h3 className="text-2xl font-bold">Description</h3>
-                <p>{job.description}</p><br></br>
+                {renderSentences(job.description)}<br></br>
                 <h3 className="text-2xl font-bold">Responsibilities</h3>
-                <p>{job.responsibilities}</p><br></br>
+                {renderSentences(job.responsibilities)}<br></br>
                 <h3 className="text-2xl font-bold">Requirements</h3>
-                <p>{job.requirements}</p><br></br>
+                {renderSentences(job.requirements)}<br></br>
             </div>
             <Link
-            href={job.link} target="_blank"
-            className="btn-primary btn"> Apply
+                href={job.link} target="_blank"
+                className="btn-primary btn"> Apply
             </Link>
         </div>
     )
